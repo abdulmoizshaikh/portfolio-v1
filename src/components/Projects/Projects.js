5ada2ffb7652ba7af5863bbded7f5e43a48e5827
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2023-03-12 17:04:16 +0500

    updates

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -45,8 +45,10 @@ const Projects = () => (
               </TagList>
             </div>
             <UtilityList>
-              <ExternalLinks href={p.visit}>Code</ExternalLinks>
-              <ExternalLinks href={p.source}>Source</ExternalLinks>
+              {/* <ExternalLinks href={p.visit}>Code</ExternalLinks> */}
+              <ExternalLinks href={p.source} target="_blank">
+                Source
+              </ExternalLinks>
             </UtilityList>
           </BlogCard>
         );
