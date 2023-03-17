8eb5e7cdbbaee9b7296ed6e1c5e8caabaf0b98b9
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2023-03-17 23:33:17 +0500

    installing google fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,16 @@
 import Theme from '../styles/theme';
+import { Roboto } from '@next/font/google';
 
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: [400, 700],
+});
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className={roboto.className}>
       <Theme>
         <Component {...pageProps} />
       </Theme>
-    </>
+    </main>
   );
 }
- 
