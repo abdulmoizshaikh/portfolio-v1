69965a31106158eb31e4f508178fabd15a3901cc
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2023-03-27 20:29:39 +0500

    updates

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -9,8 +9,8 @@ export const Img = styled.img`
 
 export const GridContainer = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-  padding: 3rem;
+  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+  // padding: 3rem;
   place-items: center;
   column-gap: 2rem;
   row-gap: 3rem;
@@ -22,10 +22,11 @@ export const GridContainer = styled.section`
   }
 `;
 export const BlogCard = styled.div`
+  min-height: 720px;
   border-radius: 10px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
-  width: 400px;
+  width: 500px;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
